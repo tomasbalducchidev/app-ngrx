4c0e39071623b06323cb0ce5a140e3bbd5c79d7b
Tomas Balducchi <tomasbalducchi@gmail.com>
2024-10-14 08:53:56 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'app-ngrx';
